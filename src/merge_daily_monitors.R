c4886d82ec1d79797042813594a2f1728dc5c005
maiktreya <miguel.garcia.duch@gmail.com>
2024-11-03 22:19:22 +0100

    last

--- src/merge_daily_monitors.R
+++ src/merge_daily_monitors.R
@@ -10,27 +10,27 @@ source("src/etl_function.R") # import needed piping logic
 data_path <- "out" # define path to data folder storing daily monitors
 stacked_data <- stack_housing_data(data_path) # extract data from base files
 
-# coerce to numeric Airbnb price column
-for (i in 1:2) stacked_data[[i]][, price_with_tax := as.numeric(gsub("[^0-9.]", "", price_with_tax))]
+# coerce to numeric Airbnb price columns
+for (i in 1:2) stacked_data[[i]][, price := as.numeric(gsub("[^0-9.]", "", price_with_tax))][, price_with_tax := NULL]
 
 # Create a new workbook
 wb <- createWorkbook()
 
 # Add each data.table to a separate sheet
 addWorksheet(wb, "Airbnb Short-Term")
-writeDataTable(wb, "Airbnb Short-Term", stacked_data$airbnb_short)
+writeData(wb, "Airbnb Short-Term", stacked_data$airbnb_short)
 
 addWorksheet(wb, "Airbnb Med-Term")
-writeDataTable(wb, "Airbnb Med-Term", stacked_data$airbnb_med)
+writeData(wb, "Airbnb Med-Term", stacked_data$airbnb_med)
 
 addWorksheet(wb, "Airbnb Long-Term")
-writeDataTable(wb, "Airbnb Long-Term", stacked_data$airbnb_long)
+writeData(wb, "Airbnb Long-Term", stacked_data$airbnb_long)
+
+addWorksheet(wb, "Idealista Buy")
+writeData(wb, "Idealista Buy", stacked_data$idealista_rent)
 
 addWorksheet(wb, "Idealista Rent")
-writeDataTable(wb, "Idealista Rent", stacked_data$idealista_rent)
-
-addWorksheet(wb, "Idealista Sell")
-writeDataTable(wb, "Idealista Sell", stacked_data$idealista_sell)
+writeData(wb, "Idealista Rent", stacked_data$idealista_sell)
 
 # Save the workbook
 saveWorkbook(wb, "stacked_housing_data.xlsx", overwrite = TRUE)
