d8faa3642327752248509ab6c5f8061445bfab50
il    iciuv <miguel.garcia.duch@gmail.com>
2025-03-21 00:12:04 +0100

    cleaned empty airbnb files

--- src/merge_daily_monitors.R
+++ src/merge_daily_monitors.R
@@ -10,6 +10,12 @@ source("src/etl_function.R") # import needed piping logic
 data_path <- "out" # define path to data folder storing daily monitors
 stacked_data <- stack_housing_data(data_path) # extract data from base files
 
+# Remove empty data.tables from the list
+stacked_data <- stacked_data[sapply(stacked_data, function(x) nrow(x) > 0)]
+
+# Check if there are any data.tables left after filtering
+if (length(stacked_data) == 0) stop("No non-empty data.tables found.")
+
 # coerce to numeric Airbnb price columns & drop properties outside target city
 for (i in 1:3) {
     stacked_data[[i]][, price := as.numeric(gsub("[^0-9.]", "", price_with_tax))][, price_with_tax := NULL]
@@ -18,22 +24,27 @@ for (i in 1:3) {
 
 # Create a new workbook
 wb <- createWorkbook()
-
-# Add each data.table to a separate sheet
-addWorksheet(wb, "Airbnb Short-Term")
-writeData(wb, "Airbnb Short-Term", stacked_data$airbnb_short)
-
-addWorksheet(wb, "Airbnb Med-Term")
-writeData(wb, "Airbnb Med-Term", stacked_data$airbnb_med)
-
-addWorksheet(wb, "Airbnb Long-Term")
-writeData(wb, "Airbnb Long-Term", stacked_data$airbnb_long)
-
-addWorksheet(wb, "Idealista Buy")
-writeData(wb, "Idealista Buy", stacked_data$idealista_rent)
-
-addWorksheet(wb, "Idealista Rent")
-writeData(wb, "Idealista Rent", stacked_data$idealista_sell)
+# Add each non-empty data.table to a separate sheet
+if ("airbnb_short" %in% names(stacked_data)) {
+    addWorksheet(wb, "Airbnb Short-Term")
+    writeData(wb, "Airbnb Short-Term", stacked_data$airbnb_short)
+}
+if ("airbnb_med" %in% names(stacked_data)) {
+    addWorksheet(wb, "Airbnb Med-Term")
+    writeData(wb, "Airbnb Med-Term", stacked_data$airbnb_med)
+}
+if ("airbnb_long" %in% names(stacked_data)) {
+    addWorksheet(wb, "Airbnb Long-Term")
+    writeData(wb, "Airbnb Long-Term", stacked_data$airbnb_long)
+}
+if ("idealista_rent" %in% names(stacked_data)) {
+    addWorksheet(wb, "Idealista Buy")
+    writeData(wb, "Idealista Buy", stacked_data$idealista_rent)
+}
+if ("idealista_sell" %in% names(stacked_data)) {
+    addWorksheet(wb, "Idealista Rent")
+    writeData(wb, "Idealista Rent", stacked_data$idealista_sell)
+}
 
 # Save the workbook
 saveWorkbook(wb, "stacked_housing_data.xlsx", overwrite = TRUE)
