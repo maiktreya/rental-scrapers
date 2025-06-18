089fd0b33a20d420a8bd45e136602d4edaefc20c
il    iciuv <miguel.garcia.duch@gmail.com>
2025-06-18 07:12:30 +0200

    Add Idealista scraper script for Segovia sales and rentals

---
+++ src/run_idealista.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# for headless and server use: install xvdf
+export DISPLAY=:99 # Make sure to use the same display where Xvfb is running
+
+# Define the path variable
+BASE_PATH="/home/other/dev/github/rental-scrapers"
+
+# Get the format parameter from the user, default to "csv" if not provided
+format=${1:-csv}
+
+# Activate virtual environment
+source "$BASE_PATH/env/bin/activate"
+
+run_scraper() {
+    local format=$1
+
+    echo "Running Idealista Segovia Sale Scraper for format: $format"
+    "$BASE_PATH/env/bin/python" "$BASE_PATH/src/idealista_scraper.py" \
+        --url "https://www.idealista.com/venta-viviendas/segovia-segovia/" \
+        --delay 5 --format "$format"
+    echo "Finished scraping Segovia Sale. Waiting 5 minutes..."
+
+    # Sleep for 5 minutes (300 seconds)
+    sleep 300
+
+    echo "Running Idealista Segovia Rent Scraper for format: $format"
+    "$BASE_PATH/env/bin/python" "$BASE_PATH/src/idealista_scraper.py" \
+        --url "https://www.idealista.com/alquiler-viviendas/segovia-segovia/" \
+        --delay 5 --format "$format"
+    echo "Finished scraping Segovia Rent."
+}
+
+# Check format option and run accordingly
+run_scraper "$format"
