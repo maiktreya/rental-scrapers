8cd746ed27b3e81e1c3a0f81775b7dd4f468cb68
maiktreya <miguel.garcia.duch@gmail.com>
2025-06-18 11:01:18 +0200

    Refactor Idealista scraper: update virtual environment path, enhance requirements, and implement new scraping logic with Playwright

--- src/run_idealista.sh
+++ src/run_idealista.sh
@@ -4,28 +4,28 @@
 export DISPLAY=:99 # Make sure to use the same display where Xvfb is running
 
 # Define the path variable
-BASE_PATH="/home/other/dev/github/rental-scrapers"
+BASE_PATH="/c/Users/70254057/Desktop/basic-hub/rental-scrapers"
 
 # Get the format parameter from the user, default to "csv" if not provided
 format=${1:-csv}
 
 # Activate virtual environment
-source "$BASE_PATH/env/bin/activate"
+source "$BASE_PATH/.venv\Scripts\activate"
 
 run_scraper() {
     local format=$1
 
     echo "Running Idealista Segovia Sale Scraper for format: $format"
-    "$BASE_PATH/env/bin/python" "$BASE_PATH/src/idealista_scraper.py" \
+    "$BASE_PATH/.venv/Scripts/python.exe" "$BASE_PATH/src/idealista_scraper.v2.py" \
         --url "https://www.idealista.com/venta-viviendas/segovia-segovia/" \
-        --delay 3.5 --format "$format"
+        --delay 5 --format "$format"
     echo "Finished scraping Segovia Sale. Waiting 5 minutes..."
 
     # Sleep for 5 minutes (300 seconds)
     sleep 300
 
     echo "Running Idealista Segovia Rent Scraper for format: $format"
-    "$BASE_PATH/env/bin/python" "$BASE_PATH/src/idealista_scraper.py" \
+    "$BASE_PATH/.venv/Scripts/python.exe" "$BASE_PATH/src/idealista_scrape.v2.py" \
         --url "https://www.idealista.com/alquiler-viviendas/segovia-segovia/" \
         --delay 5 --format "$format"
     echo "Finished scraping Segovia Rent."
